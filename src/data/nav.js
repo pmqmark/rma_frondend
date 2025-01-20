9a5345da5864cfd4234362589fca634ff8ba59ef
badusha5618 <badushaperingala@gmail.com>
2025-01-20 12:41:40 +0530

    changes

--- src/data/nav.js
+++ src/data/nav.js
@@ -1,65 +1,65 @@
 export const navBar =  [
-    {
-      "id": 1,
-      "title": "Home",
-      "link": "/",
-      "list": []
-    },
-    {
-      "id": 2,
-      "title": "About",
-      "link": "/user/about",
-      "list": []
-    },
-    {
-      "id": 3,
-      "title": "Media",
-      "link": "/user/media",
-      "list": [
-        {
-          "lid": 1,
-          "subTitle":"RMA",
-          "subLink":"/user/rma"
-        },
-        {
-          "lid": 2,
-          "subTitle":"activities",
-          "subLink":"/user/activities"
-        },
-        // {
-        //   "lid": 3,
-        //   "subTitle":"publications",
-        //   "subLink":"/user/publication"
-        // },
-        // {
-        //   "lid": 4,
-        //   "subTitle":"newsletters",
-        //   "subLink":"/user/newsletter"
-        // },
-      ]
-    },
-    {
-      "id": 4,
-      "title": "RMA Initiatives",
-      "link": "/user/wso-initiatives",
-      "list": [
-        {
-          "lid": 1,
-          "subTitle":"rules",
-          "subLink":"/user/rules"
-        },
-        // {
-        //   "lid": 2,
-        //   "subTitle":"NSSP",
-        //   "subLink":"/user/nssp"
-        // }
-      ]
-    },
-    {
-      "id": 5,
-      "title": "Contact Us",
-      "link": "/contact-us",
-      "list": []
-    }
-  ]
+  {
+    "id": 1,
+    "title": "Home",
+    "link": "/",
+    "list": []
+  },
+  {
+    "id": 2,
+    "title": "About",
+    "link": "/user/about",
+    "list": []
+  },
+  {
+    "id": 3,
+    "title": "Media",
+    "link": "/user/media",
+    "list": [
+      {
+        "lid": 1,
+        "subTitle":"RMA",
+        "subLink":"/user/rma"
+      },
+      {
+        "lid": 2,
+        "subTitle":"activities",
+        "subLink":"/user/activities"
+      },
+      // {
+      //   "lid": 3,
+      //   "subTitle":"publications",
+      //   "subLink":"/user/publication"
+      // },
+      // {
+      //   "lid": 4,
+      //   "subTitle":"newsletters",
+      //   "subLink":"/user/newsletter"
+      // },
+    ]
+  },
+  {
+    "id": 4,
+    "title": "RMA Initiatives",
+    "link": "/user/wso-initiatives",
+    "list": [
+      {
+        "lid": 1,
+        "subTitle":"rules",
+        "subLink":"/user/rules"
+      },
+      // {
+      //   "lid": 2,
+      //   "subTitle":"NSSP",
+      //   "subLink":"/user/nssp"
+      // }
+    ]
+  },
+  {
+    "id": 5,
+    "title": "Contact Us",
+    "link": "/contact-us",
+    "list": []
+  }
+]
 
